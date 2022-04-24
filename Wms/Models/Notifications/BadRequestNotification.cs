35aa4df2b0ebfb86d0486a1f6af7853496649a5f
zhixian <zhixian@hotmail.com>
2022-04-24 19:50:29 +0800

    add notifications

---
+++ Wms/Models/Notifications/BadRequestNotification.cs
@@ -0,0 +1,14 @@
+﻿namespace Wms.Models.Notifications;
+
+public class BadRequestNotification : NotificationBase
+{
+    public BadRequestNotification()
+    {
+        this.NotificationType = NotificationType.Error;
+    }
+
+    public BadRequestNotification(string message) : this()
+    {
+        this.Message = message;
+    }
+}
---
+++ Wms/Models/Notifications/WarningNotification.cs
@@ -0,0 +1,14 @@
+﻿namespace Wms.Models.Notifications;
+
+public class WarningNotification : NotificationBase
+{
+    public WarningNotification()
+    {
+        this.NotificationType = NotificationType.Warning;
+    }
+
+    public WarningNotification(string message) : this()
+    {
+        this.Message = message;
+    }
+}
