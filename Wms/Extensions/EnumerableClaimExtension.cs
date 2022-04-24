ad832fcc63581ceed87e28fd569a6f475e7ddbf6
zhixian <zhixian@hotmail.com>
2022-04-24 19:50:41 +0800

    Add extension to map claims

---
+++ Wms/Extensions/EnumerableClaimExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace Wms.Extensions;
+
+public static class EnumerableClaimExtensions
+{
+    public static IEnumerable<Claim> Map(this IEnumerable<Claim> claims, IDictionary<string, string> claimTypeMap)
+    {
+        foreach (Claim claim in claims)
+        {
+            if (claimTypeMap.TryGetValue(claim.Type, out string? value))
+            {
+                yield return new Claim(value, claim.Value, claim.ValueType, claim.Issuer, claim.OriginalIssuer, claim.Subject);
+            }
+            else
+            {
+                yield return claim;
+            }
+        }
+    }
+}
