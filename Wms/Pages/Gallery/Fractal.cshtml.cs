7412f4c4455015067811723c5377ba7d9c303edd
zhixian <zhixian@hotmail.com>
2022-05-01 22:46:26 +0800

    update

---
+++ Wms/Pages/Gallery/Fractal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wms.Pages.Gallery
+{
+    public class FractalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
