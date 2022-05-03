efc7cce6fd0033b923e8a150c3ebc50292d04df3
zhixian <zhixian@hotmail.com>
2022-05-03 23:40:07 +0800

    add fractals

---
+++ Wms/Pages/Gallery/Koch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wms.Pages.Gallery
+{
+    public class KochModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Wms/Pages/Gallery/Seirpinski.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wms.Pages.Gallery
+{
+    public class SeirpinskiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
