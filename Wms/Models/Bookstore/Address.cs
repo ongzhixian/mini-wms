d0635eb631ec7eaf24dbc3d9baa51891eeb194e4
zhixian <zhixian@hotmail.com>
2022-05-05 22:19:23 +0800

    common type

---
+++ Wms/Models/Bookstore/Address.cs
@@ -0,0 +1,18 @@
+﻿namespace Wms.Models.Bookstore;
+
+public class Address
+{
+    public string? Street { get; set; }
+
+    public string? Unit { get; set; }
+
+    public string? City { get; set; }
+
+    // We use reegion to represent state/province
+    public string? Region { get; set; }
+
+    // Postal code
+    public string? PostCode { get; set; }
+
+    public string? Country { get; set; }
+}
---
+++ Wms/Models/Bookstore/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace Wms.Models.Bookstore;
+
+public class Contact
+{
+    public string ContactType { get; set; }
+
+    public string Value { get; set; }
+}
