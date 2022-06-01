f51876c9d8e4d69fd87740520960045e56c87588
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-06-01 12:17:10 +0800

    add some test pages

---
+++ Wms/Pages/First.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wms.Pages;
+
+public class FirstModel : PageModel
+{
+    private readonly ILogger<FirstModel> _logger;
+
+    public FirstModel(ILogger<FirstModel> logger)
+    {
+        _logger = logger;
+    }
+}
---
+++ Wms/Pages/Second.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wms.Pages;
+
+public class SecondModel : PageModel
+{
+    private readonly ILogger<SecondModel> _logger;
+
+    public SecondModel(ILogger<SecondModel> logger)
+    {
+        _logger = logger;
+    }
+}
